050aaf6458df694821c929fc850850f8387eca59
DatCorno <corno_@live.ca>
2013-08-26 14:11:30 -0400

    Ajout de la classe Buttons, pour la dégénéraliser de MainMenu. Aucun changement de la classe

---
+++ TD/TD/Buttons.cs
@@ -0,0 +1,32 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace TD
+{
+    class Buttons
+    {
+        public Rectangle spacePos { get; set; }
+        public Texture2D texture { get; set; }
+        public Color couleur { get; set; }
+        public float Transparency { get; set; }
+        public GameState returnState { get; set; }
+        public Buttons(Rectangle _spacePos, Texture2D _texture, Color _couleur, GameState _return)
+        {
+            spacePos = _spacePos;
+            couleur = _couleur;
+            texture = _texture;
+            returnState = _return;
+            Transparency = 1.0f;
+        }
+    }
+}
