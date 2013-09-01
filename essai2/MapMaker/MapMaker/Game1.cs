5b7fc1e085e9fefa0f81539b1eb9a390c3b73d85
Raphaël Côté <cotlarrc@msn.com>
2013-09-01 03:18:11 -0400

    mapmaker

---
+++ essai2/MapMaker/MapMaker/Game1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace MapMaker
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    enum ClickState { Clicked, Held, Releasing, Released }
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        public static GraphicsDeviceManager graphics;
+        List<Cell> changed;
+        SpriteBatch spriteBatch;
+        MouseHandler mouse;
+        Camera cam;
+        Grille grid;
+        public static Texture2D texture;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            IsMouseVisible = true;
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            texture = Content.Load<Texture2D>("Cell");
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            grid = new Grille();
+            cam = new Camera(grid);
+            mouse = new MouseHandler();
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+            if (mouse.LeftClickState == ClickState.Clicked || mouse.LeftClickState == ClickState.Held)
+            {
+                foreach (var item in grid.cellules)
+                {
+                    if (item.position.Contains(mouse.fakePos))
+                    {
+                        item.SwitchKind();
+                    }
+                }
+            }
+            else if (mouse.LeftClickState == ClickState.Releasing)
+            {
+
+            }
+
+            cam.Update(mouse, gameTime);
+            mouse.Update(cam);
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+        protected override void OnExiting(object sender, EventArgs args)
+        {
+
+            base.OnExiting(sender, args);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null, cam.viewMatrix);
+            grid.Draw(spriteBatch);
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ essai2/MapMaker/MapMaker/Grille.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapMaker
+{
+    class Grille
+    {
+        public Cell[,] cellules = new Cell[100, 100];
+        public Grille()
+        {
+            for (int i = 0; i < cellules.GetLength(0); i++)
+            {
+                for (int j = 0; j < cellules.GetLength(1); j++)
+                {
+                    cellules[i,j] = new Cell(j, i);
+                }
+            }
+        }
+        public void Draw(SpriteBatch sprite)
+        {
+            foreach (var item in cellules)
+            {
+                item.Draw(sprite);
+            }
+        }
+    }
+}
